ccca2a24e1f312e62ea44c6b8f8e8fd97e66f44a
Steven Knudsen <knud@ualberta.ca>
2024-06-09 09:20:47 -0600

    Test the reception of samples from the RPi Pico emulating the PEPPER FPGA. Works for samples sent at 6 MHz SPI clock.
    Samples are read dirctly into a buffer using the STM SPI blocking
    read.
    
    Since the PEPPER ADC is 24 bit and a design choice for the FPGA
    was to output 32 bits per SPI word, only the first three bytes
    in the receive data are valid. To keep the design simple, we
    decided to capture all 4 bytes and save them instead of pruning
    them.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -74,38 +74,38 @@ static void MX_SPI3_Init(void);
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
 
-	/* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_SPI1_Init();
-	MX_FATFS_Init();
-	MX_SPI3_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_FATFS_Init();
+  MX_SPI3_Init();
+  /* USER CODE BEGIN 2 */
 	//  myprintf("\r\n~ SD card demo ~\r\n\r\n");
 	HAL_Delay(500);
 
@@ -114,288 +114,299 @@ int main(void)
 	FRESULT fres;
 
 	uint32_t start_ms, diff_ms;
-	char filename[15];
-
-	enum SD_CARD_TEST {
-		WRITE_10MB_FILE,
-		CREATE_500_FILES,
-		CREATE_100_1MB_FILES
-	};
-
-	enum SD_CARD_TEST theTest = WRITE_10MB_FILE;
-
-	fres = f_mount(&FatFs, "", 1);
-	if (fres != FR_OK) {
-		//    myprintf("f_mount error (%i) \r\n", fres);
-		while (1);
-	}
-
-	switch (theTest) {
-	case WRITE_10MB_FILE:
-		uint32_t byteCount = 0;
-		// Create test file on SD card
-		fres = f_open(&fil, "ten_megabyte_file.bin", FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
-		if (fres == FR_OK) {
-			//			uint32_t faux_sample1 = 0xDEADBEEF;
-			//			uint32_t faux_sample2 = 0xCAFEBABE;
-
-			uint32_t bufferKB = 48;
-			uint32_t numBytes = bufferKB*220*1024;
-			// There is 64 kB of RAM to work with. Use 48 kB
-			uint32_t numSamples = bufferKB*1024/4;
-			uint32_t samples[numSamples];
-			for (uint16_t j=0; j< numSamples; j+=2) {
-				samples[j] = 0xDEADBEEF;
-				samples[j+1] = 0xCAFEBABE;
-			}
-
-			start_ms = HAL_GetTick();
-
-			// Test time to write ~10 MB
-			uint32_t numIter = numBytes / (bufferKB*1024);
-			for (uint16_t i = 0; i < numIter; i++) {
-				fres = f_write(&fil, samples, numSamples*4, &bytesSaved);
-				byteCount += numSamples*4;
-				if (fres != FR_OK) {
-					// Should really do something here, but can't use printf. Blink an LED?
-				}
-			}
-			diff_ms = HAL_GetTick() - start_ms;
-		}
-
-		// Close file
-		f_close(&fil);
-		break;
-	case CREATE_500_FILES:
-		// Test time to open new file and close new file
-
-		start_ms = HAL_GetTick();
-
-		for (int i = 0; i < 500; i++) {
-			sprintf(filename,"file_%d.bin",i);
-			fres = f_open(&fil, filename, FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
-			if (fres != FR_OK) {
-				// Should really do something here, but can't use printf. Blink an LED?
-			}
-			// Close file
-			f_close(&fil);
-		}
-		diff_ms = HAL_GetTick() - start_ms;
-		break;
-	case CREATE_100_1MB_FILES:
-
-		start_ms = HAL_GetTick();
-
-		for (int i = 0; i < 100; i++) {
-			sprintf(filename,"1MB_%d.bin",i);
-			fres = f_open(&fil, filename, FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
-			if (fres == FR_OK) {
-				uint32_t faux_sample1 = 0xF00D4FEE;
-				uint32_t faux_sample2 = 0xBEC0FFEE;
-				UINT bytesSaved;
-				for (uint32_t i = 0; i < 1024*1024/8; i++) {
-					fres = f_write(&fil, &faux_sample1, 4, &bytesSaved);
-					fres = f_write(&fil, &faux_sample2, 4, &bytesSaved);
-					if (fres != FR_OK) {
-						// Should really do something here, but can't use printf. Blink an LED?
-					}
-				}
-			}
-			// Close file
-			f_close(&fil);
-		}
-		diff_ms = HAL_GetTick() - start_ms;
-		break;
-	default :
-		break;
-	}
+//	char filename[15];
+//
+//	enum SD_CARD_TEST {
+//		WRITE_10MB_FILE,
+//		CREATE_500_FILES,
+//		CREATE_100_1MB_FILES
+//	};
+//
+//	enum SD_CARD_TEST theTest = WRITE_10MB_FILE;
+//
+//	fres = f_mount(&FatFs, "", 1);
+//	if (fres != FR_OK) {
+//		//    myprintf("f_mount error (%i) \r\n", fres);
+//		while (1);
+//	}
+//
+//	switch (theTest) {
+//	case WRITE_10MB_FILE:
+//		uint32_t byteCount = 0;
+//		// Create test file on SD card
+//		fres = f_open(&fil, "ten_megabyte_file.bin", FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
+//		if (fres == FR_OK) {
+//			//			uint32_t faux_sample1 = 0xDEADBEEF;
+//			//			uint32_t faux_sample2 = 0xCAFEBABE;
+//
+//			uint32_t bufferKB = 48;
+//			uint32_t numBytes = bufferKB*220*1024;
+//			// There is 64 kB of RAM to work with. Use 48 kB
+//			uint32_t numSamples = bufferKB*1024/4;
+//			uint32_t samples[numSamples];
+//			for (uint16_t j=0; j< numSamples; j+=2) {
+//				samples[j] = 0xDEADBEEF;
+//				samples[j+1] = 0xCAFEBABE;
+//			}
+//
+//			start_ms = HAL_GetTick();
+//
+//			// Test time to write ~10 MB
+//			uint32_t numIter = numBytes / (bufferKB*1024);
+//			for (uint16_t i = 0; i < numIter; i++) {
+//				fres = f_write(&fil, samples, numSamples*4, &bytesSaved);
+//				byteCount += numSamples*4;
+//				if (fres != FR_OK) {
+//					// Should really do something here, but can't use printf. Blink an LED?
+//				}
+//			}
+//			diff_ms = HAL_GetTick() - start_ms;
+//		}
+//
+//		// Close file
+//		f_close(&fil);
+//		break;
+//	case CREATE_500_FILES:
+//		// Test time to open new file and close new file
+//
+//		start_ms = HAL_GetTick();
+//
+//		for (int i = 0; i < 500; i++) {
+//			sprintf(filename,"file_%d.bin",i);
+//			fres = f_open(&fil, filename, FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
+//			if (fres != FR_OK) {
+//				// Should really do something here, but can't use printf. Blink an LED?
+//			}
+//			// Close file
+//			f_close(&fil);
+//		}
+//		diff_ms = HAL_GetTick() - start_ms;
+//		break;
+//	case CREATE_100_1MB_FILES:
+//
+//		start_ms = HAL_GetTick();
+//
+//		for (int i = 0; i < 100; i++) {
+//			sprintf(filename,"1MB_%d.bin",i);
+//			fres = f_open(&fil, filename, FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
+//			if (fres == FR_OK) {
+//				uint32_t faux_sample1 = 0xF00D4FEE;
+//				uint32_t faux_sample2 = 0xBEC0FFEE;
+//				UINT bytesSaved;
+//				for (uint32_t i = 0; i < 1024*1024/8; i++) {
+//					fres = f_write(&fil, &faux_sample1, 4, &bytesSaved);
+//					fres = f_write(&fil, &faux_sample2, 4, &bytesSaved);
+//					if (fres != FR_OK) {
+//						// Should really do something here, but can't use printf. Blink an LED?
+//					}
+//				}
+//			}
+//			// Close file
+//			f_close(&fil);
+//		}
+//		diff_ms = HAL_GetTick() - start_ms;
+//		break;
+//	default :
+//		break;
+//	}
 
 	// unmount the drive
-	f_mount(NULL, "", 0);
+//	f_mount(NULL, "", 0);
 
-	/* USER CODE END 2 */
+//	  char spi_buf[4];
+	  uint8_t longBuf[300];
+//	  for (int j = 0; j < 100; j++) longBuf[j] = 0;
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+//	  for (uint16_t k = 0; k < 5; k++) {
+		  for (uint16_t j = 0; j < 300; j++) longBuf[j] = 0;
+		  for (uint16_t i = 0; i < 300; i+=4) {
+			  HAL_SPI_Receive(&hspi3, (longBuf + i), 4, 100);
+		  }
+//		  HAL_Delay(53);
+//	  }
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1)
 	{
 		//	  printf("\r\n~ SD card demo ~\r\n\r\n");
 		//	  myprintf("\r\n~ SD card demo ~\r\n\r\n");
 		HAL_Delay(500);
-		/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-		/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 	}
-	/* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/** Configure the main internal regulator output voltage
-	 */
-	if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Configure LSE Drive Capability
-	 */
-	HAL_PWR_EnableBkUpAccess();
-	__HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+  /** Configure LSE Drive Capability
+  */
+  HAL_PWR_EnableBkUpAccess();
+  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
-	RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-	RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-	RCC_OscInitStruct.MSICalibrationValue = 0;
-	RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
-	RCC_OscInitStruct.PLL.PLLM = 1;
-	RCC_OscInitStruct.PLL.PLLN = 18;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
-	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 18;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Enable MSI Auto calibration
-	 */
-	HAL_RCCEx_EnableMSIPLLMode();
+  /** Enable MSI Auto calibration
+  */
+  HAL_RCCEx_EnableMSIPLLMode();
 }
 
 /**
- * @brief SPI1 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_SPI1_Init(void)
 {
 
-	/* USER CODE BEGIN SPI1_Init 0 */
+  /* USER CODE BEGIN SPI1_Init 0 */
 
-	/* USER CODE END SPI1_Init 0 */
+  /* USER CODE END SPI1_Init 0 */
 
-	/* USER CODE BEGIN SPI1_Init 1 */
+  /* USER CODE BEGIN SPI1_Init 1 */
 
-	/* USER CODE END SPI1_Init 1 */
-	/* SPI1 parameter configuration*/
-	hspi1.Instance = SPI1;
-	hspi1.Init.Mode = SPI_MODE_MASTER;
-	hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-	hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
-	hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-	hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-	hspi1.Init.NSS = SPI_NSS_SOFT;
-	hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
-	hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-	hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-	hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-	hspi1.Init.CRCPolynomial = 7;
-	hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-	hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
-	if (HAL_SPI_Init(&hspi1) != HAL_OK)
-	{
-		Error_Handler();
-	}
-	/* USER CODE BEGIN SPI1_Init 2 */
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 7;
+  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
 
-	/* USER CODE END SPI1_Init 2 */
+  /* USER CODE END SPI1_Init 2 */
 
 }
 
 /**
- * @brief SPI3 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief SPI3 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_SPI3_Init(void)
 {
 
-	/* USER CODE BEGIN SPI3_Init 0 */
+  /* USER CODE BEGIN SPI3_Init 0 */
 
-	/* USER CODE END SPI3_Init 0 */
+  /* USER CODE END SPI3_Init 0 */
 
-	/* USER CODE BEGIN SPI3_Init 1 */
+  /* USER CODE BEGIN SPI3_Init 1 */
 
-	/* USER CODE END SPI3_Init 1 */
-	/* SPI3 parameter configuration*/
-	hspi3.Instance = SPI3;
-	hspi3.Init.Mode = SPI_MODE_SLAVE;
-	hspi3.Init.Direction = SPI_DIRECTION_2LINES_RXONLY;
-	hspi3.Init.DataSize = SPI_DATASIZE_8BIT;
-	hspi3.Init.CLKPolarity = SPI_POLARITY_LOW;
-	hspi3.Init.CLKPhase = SPI_PHASE_1EDGE;
-	hspi3.Init.NSS = SPI_NSS_SOFT;
-	hspi3.Init.FirstBit = SPI_FIRSTBIT_MSB;
-	hspi3.Init.TIMode = SPI_TIMODE_DISABLE;
-	hspi3.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-	hspi3.Init.CRCPolynomial = 7;
-	hspi3.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-	hspi3.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
-	if (HAL_SPI_Init(&hspi3) != HAL_OK)
-	{
-		Error_Handler();
-	}
-	/* USER CODE BEGIN SPI3_Init 2 */
+  /* USER CODE END SPI3_Init 1 */
+  /* SPI3 parameter configuration*/
+  hspi3.Instance = SPI3;
+  hspi3.Init.Mode = SPI_MODE_SLAVE;
+  hspi3.Init.Direction = SPI_DIRECTION_2LINES_RXONLY;
+  hspi3.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi3.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi3.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi3.Init.NSS = SPI_NSS_HARD_INPUT;
+  hspi3.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi3.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi3.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi3.Init.CRCPolynomial = 7;
+  hspi3.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi3.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
+  if (HAL_SPI_Init(&hspi3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI3_Init 2 */
 
-	/* USER CODE END SPI3_Init 2 */
+  /* USER CODE END SPI3_Init 2 */
 
 }
 
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_GPIO_Init(void)
 {
-	GPIO_InitTypeDef GPIO_InitStruct = {0};
-	/* USER CODE BEGIN MX_GPIO_Init_1 */
-	/* USER CODE END MX_GPIO_Init_1 */
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
 
-	/* GPIO Ports Clock Enable */
-	__HAL_RCC_GPIOC_CLK_ENABLE();
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	__HAL_RCC_GPIOB_CLK_ENABLE();
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(SD_CS_GPIO_Port, SD_CS_Pin, GPIO_PIN_RESET);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(SD_CS_GPIO_Port, SD_CS_Pin, GPIO_PIN_RESET);
 
-	/*Configure GPIO pin : SD_CS_Pin */
-	GPIO_InitStruct.Pin = SD_CS_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(SD_CS_GPIO_Port, &GPIO_InitStruct);
+  /*Configure GPIO pin : SD_CS_Pin */
+  GPIO_InitStruct.Pin = SD_CS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(SD_CS_GPIO_Port, &GPIO_InitStruct);
 
-	/* USER CODE BEGIN MX_GPIO_Init_2 */
-	/* USER CODE END MX_GPIO_Init_2 */
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -403,33 +414,33 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
 	{
 	}
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-	/* USER CODE BEGIN 6 */
+  /* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-	/* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
